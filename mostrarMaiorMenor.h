dbdacbaacee9c30de4611333670e5c8eb4ee97d0
CarvalhoAlisson <131178151+CarvalhoAlisson@users.noreply.github.com>
2023-04-18 11:38:07 -0300

    Add files via upload

---
+++ mostrarMaiorMenor.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+void MaiorMenor(int maior, int menor)
+{
+	cout << "\t\t***Mostra maior e menor***\n";
+	cout << "\t\t\tMenor: " << menor << "\n";
+	cout << "\t\t\tMaior: " << maior << "\n";
+	cout << "\t\t***************************\n\n";
+	Sleep(1000);
+}
